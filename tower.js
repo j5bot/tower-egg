1d051cfd58d3a45667929f89b3ad60d9447bc35b
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2013-04-17 17:49:05 -0400

    Updated styles to fix radial gradient

--- tower.js
+++ tower.js
@@ -1,5 +1,13 @@
-(function ($) {
-	var STYLE_SHEET = ".tower-container{position:relative;width:800px;height:280px;background-color:white;border:#3d9952 solid 3px;font-family:Helvetica,Arial,sans-serif;border-radius:20px;z-index:20;user-select:none;-webkit-user-select:none}.tower-banner{display:none;position:absolute;top:50px;left:200px;height:100px;width:400px;font-size:12px;background-color:#3d9952;color:white;text-align:center;padding:20px;border-radius:20px;border:solid 1px #c6f1a2;z-index:24}.tower-banner h1{font-size:30px}.tower-peg{display:inline-block;position:relative;margin:0;padding:0;margin-left:10px;width:250px;height:280px;background-image:-webkit-linear-gradient(left,transparent 52px,#53c66d 52px,#c6f1a2 72px,#53c66d 84px,transparent 88px);background-image:linear-gradient(left,transparent 25%,#c6f1a2 26%,#53c66d 34%,transparent 36%)}.tower-peg-top{height:30px;width:60px;position:absolute;top:0;left:50px;background-color:white;background-image:-webkit-radial-gradient(20px bottom,20px 13px,#c6f1a2 0,#53c66d 88%,rgba(0,0,0,0) 88%);background-image:radial-gradient(10% 10% at 25% bottom,#c6f1a2 22%,#3d9952 88%,rgba(0,0,0,0) 88%)}.tower-discs{list-style-type:none;margin:0;padding:0;position:absolute;bottom:0;width:230px;text-align:center;z-index:23;cursor:hand}.tower-disc{display:block;color:white;font-size:15px;line-height:25px;font-weight:bold;text-shadow:-1px -1px rgba(0,0,0,0.3);background-color:#4bd321;background-image:-webkit-linear-gradient(top,#c6f1a2 22%,#3d9952 88%);background-image:-moz-linear-gradient(top,#c6f1a2 22%,#3d9952 88%);background-image:-ms-linear-gradient(top,#c6f1a2 22%,#3d9952 88%);background-image:-o-linear-gradient(top,#c6f1a2 22%,#3d9952 88%);background-image:linear-gradient(to bottom,#c6f1a2 22%,#3d9952 88%);border:solid 1px #3d9952;border-radius:15px;margin:10px;height:25px}";
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery', 'jquery-ui'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+	var STYLE_SHEET = ".tower-container{position:relative;width:800px;height:280px;background-color:white;border:#3d9952 solid 3px;font-family:Helvetica,Arial,sans-serif;border-radius:20px;z-index:20;user-select:none;-webkit-user-select:none;}.tower-banner{display:none;position:absolute;top:50px;left:200px;height:100px;width:400px;font-size:12px;background-color:#3d9952;color:white;text-align:center;padding:20px;border-radius:20px;border:solid 1px #c6f1a2;z-index:24;}.tower-banner h1{font-size:30px;}.tower-peg{display:inline-block;position:relative;margin:0;padding:0;margin-left:10px;width:250px;height:280px;background-image:-webkit-linear-gradient(left,transparent 52px,#53c66d 52px,#c6f1a2 72px,#53c66d 84px,transparent 88px);background-image:linear-gradient(left,transparent 52px,#53c66d 52px,#c6f1a2 72px,#53c66d 84px,transparent 88px);}.tower-peg-top{height:30px;width:60px;position:absolute;top:0;left:50px;background-color:white;background-image:-webkit-radial-gradient(20px bottom,20px 13px,#c6f1a2 0,#53c66d 88%,rgba(0,0,0,0) 88%);background-image:radial-gradient(20px 13px at 20px bottom,#c6f1a2 0,#53c66d 88%,rgba(0,0,0,0) 88%);}.tower-discs{list-style-type:none;margin:0;padding:0;position:absolute;bottom:0;width:230px;text-align:center;z-index:23;cursor:hand;}.tower-disc{display:block;color:white;font-size:15px;line-height:25px;font-weight:bold;text-shadow:-1px -1px rgba(0,0,0,0.3);background-color:#4bd321;background-image:-webkit-linear-gradient(top,#c6f1a2 22%,#3d9952 88%);background-image:-moz-linear-gradient(top,#c6f1a2 22%,#3d9952 88%);background-image:-ms-linear-gradient(top,#c6f1a2 22%,#3d9952 88%);background-image:-o-linear-gradient(top,#c6f1a2 22%,#3d9952 88%);background-image:linear-gradient(to bottom,#c6f1a2 22%,#3d9952 88%);border:solid 1px #3d9952;border-radius:15px;margin:10px;height:25px;}";
 	/**
 	 * Create a Lucas' Tower / Tower of Hanoi game in a container on the page
 	 *
@@ -228,4 +236,4 @@
 		return this;
 	};
 
-}(jQuery));
+}));
